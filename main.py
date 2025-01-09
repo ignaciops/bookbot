b96362648211929f9af3e482452e2c93c8004461
ig <ignacio@perezsmith.me>
2025-01-09 13:41:14 -0600

    Updated main with get_report function

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ def main():
     text = get_book_text(book_path)
     num_words = get_book_words(text)
     chars_dict = get_character_appearances(text)
-    print(chars_dict)
+    get_report(book_path, num_words, chars_dict)
+    
 
 def get_character_appearances(text):
     lower_text = text.lower()
     characters = {}
-    for s in text:
+    for s in lower_text:
         character = s
         if character not in characters:
             characters[character] = 1
@@ -23,5 +24,29 @@ def get_book_text(path):
 def get_book_words(book):
     words = book.split()
     return len(words)
+
+def get_report(book_path, word_count, char_dict):
+    # Data preparation
+    header = f"--- Begin report of {book_path} ---"
+    count = f"{word_count} words found in the document"
+    letters = {key: value for key, value in char_dict.items() if key.isalpha()}
+    letters_list = [{"char": char, "num": num} for char, num in letters.items()]
+    
+    # Helper function
+    def sort_on(dict):
+        return dict["num"]
+    
+    # Processing data
+    letters_list.sort(reverse=True, key=sort_on)
+    
+    # Output
+    print(header)
+    print(count)
+    for letter in letters_list:
+        char = letter["char"]
+        num = letter["num"]
+        print(f"The '{char}' character was found {num} times")
+    print("--- End report ---")
+
 
 main()
